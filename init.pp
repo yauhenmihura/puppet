e973eec4dea6d49812025e0dfb0acc1ade9adc88
yauhenmihura <yauhen_mihura@epam.com>
2016-09-22 15:55:21 +0300

    updated init.pp

--- init.pp
+++ init.pp
@@ -1,25 +1,48 @@
 # /init.pp:
 
-if ${hostname} == 'node1' {
+if $::hostname == 'node1' {
 
-   exec { "master_ip":
-     command =>
-       'if ! grep master.mihura.com /etc/hosts ; then
-          echo "192.168.33.14 master.mihura.com master" >> /etc/hosts
-       fi',
-   }
+  exec { 'master_dns':
+    command =>
+      'if ! grep master.mihura.com /etc/hosts ; then
+        echo "192.168.33.14 master.mihura.com master" >> /etc/hosts
+      fi',
+    provider => shell,
+  }
  
-   package { 'puppet-agent':
-     ensure => 'latest',
-   }
+  package { 'puppet-agent':
+    ensure => 'latest',
+  }
 
-   service { 'puppet':
-     ensure => 'running',
-     enable => 'true',
-   }
+  exec { 'master_server':
+    command =>
+      'if ! grep master.mihura.com /etc/puppetlabs/puppet/puppet.conf; then
+        echo "server =  master.mihura.com" >> /etc/puppetlabs/puppet/puppet.conf
+      fi',
+    provider => shell,
+  }
+
+  service { 'puppet':
+    ensure => 'running',
+    enable => 'true',
+  }
 }
-#                                   #  exec { "master_ip":
-#                                   #    command => 'echo "master.mihura.com master" >> /etc/hosts',
-#                                   #    onlyif  => "grep "192.168.33.14 master.mihura.com" /etc/hosts",
-#                                   #  }
-#
+
+elsif $::hostname == 'master' {
+
+  exec { 'node1_dns':
+    command =>
+      'if ! grep node1.mihura.com /etc/hosts ; then
+        echo "192.168.33.13 node1.mihura.com node1" >> /etc/hosts
+      fi',
+    provider => shell,
+  }
+
+  package { 'puppetserver':
+    ensure => 'latest',
+  }
+  
+  service { 'puppetserver': 
+    ensure => 'running',
+  }
+}
