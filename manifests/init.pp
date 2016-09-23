3b955696979337c870f7afd8bb8150d19681cea7
yauhenmihura <yauhen_mihura@epam.com>
2016-09-23 11:58:09 +0300

    changes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,26 +1,18 @@
-# manifests/init.pp:
+# /init.pp:
 
 if $::hostname == 'node1' {
 
-  exec { 'master_dns':
-    command =>
-      'if ! grep master.mihura.com /etc/hosts ; then
-        echo "192.168.33.14 master.mihura.com master" >> /etc/hosts
-      fi',
-    provider => shell,
-  }
- 
-  package { 'puppet-agent':
-    ensure => 'latest',
+  host { 'master.mihura.com':
+    ip           => '192.168.33.14',
+    host_aliases => 'master',
   }
 
-  exec { 'master_server':
-    command =>
-      'if ! grep master.mihura.com /etc/puppetlabs/puppet/puppet.conf; then
-        echo "server =  master.mihura.com" >> /etc/puppetlabs/puppet/puppet.conf
-      fi',
-    provider => shell,
-  }
+  file { '/etc/puppetlabs/puppet/puppet.conf':
+    content => template('/vagrant/templates/node/puppet.conf.erb'),
+    owner   => root,
+    group   => root,
+    mode => '0644'
+   }
 
   service { 'puppet':
     ensure => 'running',
@@ -30,25 +22,21 @@ if $::hostname == 'node1' {
 
 elsif $::hostname == 'master' {
 
-  exec { 'node1_dns':
-    command =>
-      'if ! grep node1.mihura.com /etc/hosts ; then
-        echo "192.168.33.13 node1.mihura.com node1" >> /etc/hosts
-      fi',
-    provider => shell,
+  host { 'node1.mihura.com':
+    ip           => '192.168.33.13',
+    host_aliases => 'node1',
   }
 
   package { 'puppetserver':
     ensure => 'latest',
   }
 
-  exec { 'domain_autosign':
-    command =>
-      'if ! grep *.mihura.com /etc/hosts ; then
-        echo "*.mihura.com" >> /etc/puppetlabs/puppet/autosign.conf
-      fi',
-    provider => shell,
-  }
+  file { '/etc/puppetlabs/puppet/autosign.conf':
+    content => template('/vagrant/templates/master/autosign.conf.erb'),
+    owner   => root,
+    group   => root,
+    mode => '0644'
+   }
 
   service { 'puppetserver': 
     ensure => 'running',
